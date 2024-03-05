ab2232641084a521c666f03c151babe2b977f2bb
harshvirani7 <harsh.v@een.com>
2024-03-05 17:56:15 +0530

    create, update, get devices APIs working

---
+++ main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/harshvirani7/my-app/utils"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type metrics struct {
+	devices  prometheus.Gauge
+	info     *prometheus.GaugeVec
+	upgrades *prometheus.CounterVec
+}
+
+type Device struct {
+	ID       int    `json:"id"`
+	Mac      string `json:"mac"`
+	Firmware string `json:"firmware"`
+}
+
+var dvs []Device
+
+var version string
+
+func init() {
+	version = "2.10.5"
+
+	dvs = []Device{
+		{1, "5F-33-CC-1F-43-82", "2.1.6"},
+		{2, "EF-2B-C4-F5-D6-34", "2.1.6"},
+	}
+}
+
+type registerDevicesHandler struct {
+	metrics *metrics
+}
+
+func (rdh registerDevicesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getDevices(w, r)
+	case "POST":
+		createDevice(w, r, rdh.metrics)
+	default:
+		w.Header().Set("Allow", "GET, POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+type manageDevicesHandler struct {
+	metrics *metrics
+}
+
+func (mdh manageDevicesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "PUT":
+		upgradeDevice(w, r, mdh.metrics)
+	default:
+		w.Header().Set("Allow", "PUT")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func getDevices(w http.ResponseWriter, _ *http.Request) {
+	b, err := json.Marshal(dvs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func createDevice(w http.ResponseWriter, r *http.Request, m *metrics) {
+	var dv Device
+
+	err := json.NewDecoder(r.Body).Decode(&dv)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dvs = append(dvs, dv)
+
+	// m.devices.Inc()
+	m.devices.Set(float64(len(dvs)))
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Device created!"))
+}
+
+func upgradeDevice(w http.ResponseWriter, r *http.Request, m *metrics) {
+	path := strings.TrimPrefix(r.URL.Path, "/devices/")
+
+	id, err := strconv.Atoi(path)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+	}
+
+	var dv Device
+	err = json.NewDecoder(r.Body).Decode(&dv)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for i := range dvs {
+		if dvs[i].ID == id {
+			dvs[i].Firmware = dv.Firmware
+		}
+	}
+	utils.Sleep(1000)
+
+	m.upgrades.With(prometheus.Labels{"type": "router"}).Inc()
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("Upgrading..."))
+}
+
+func NewMetrics(reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		devices: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "myapp",
+			Name:      "connected_devices",
+			Help:      "Number of currently connected devices.",
+		}),
+		info: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "myapp",
+			Name:      "info",
+			Help:      "Information about the My App environment.",
+		},
+			[]string{"version"}),
+		upgrades: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Namespace: "myapp",
+			Name:      "device_upgrade_total",
+			Help:      "Number of upgraded devices.",
+		}, []string{"type"}),
+	}
+	reg.MustRegister(m.devices, m.info, m.upgrades)
+	return m
+}
+
+func main() {
+	reg := prometheus.NewRegistry()
+	m := NewMetrics(reg)
+
+	m.devices.Set(float64(len(dvs)))
+	m.info.With(prometheus.Labels{"version": version}).Set(1)
+
+	dMux := http.NewServeMux()
+	rdh := registerDevicesHandler{metrics: m}
+	mdh := manageDevicesHandler{metrics: m}
+
+	dMux.Handle("/devices", rdh)
+	dMux.Handle("/devices/", mdh)
+
+	pMux := http.NewServeMux()
+	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
+	pMux.Handle("/metrics", promHandler)
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":8080", dMux))
+	}()
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":8081", pMux))
+	}()
+
+	select {}
+}
